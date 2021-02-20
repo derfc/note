99eb3d4b2540c3095e017927cfd2870fab456e79
fred cheung <fredcheung@live.hk>
2021-02-21 01:02:33 +0800

    get post put del, without users and frontend

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var fs = require("fs");
+var Note = require("./note");
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+let jsonFile = __dirname + "/note.json";
+let noteData = new Note(jsonFile);
+var port = 8080;
+
+app.get("/note", (req, res) => {
+  console.log("getting json file");
+  return noteData
+    .list()
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/note/:id", (req, res) => {
+  res.json(noteStore[req.params.id]);
+});
+
+app.post("/note", (req, res) => {
+  console.log("posting json file");
+  let note = req.body;
+  console.log("posting ", note);
+  return noteData
+    .add(note)
+    .then(() => noteData.list())
+    .then((notes) => {
+      res.json("posted");
+      res.status(200).json(notes);
+    });
+});
+
+app.put("/note/:index", (req, res) => {
+  console.log("updating json file");
+  let index = req.params.index;
+  console.log("index: ", index);
+  let newNote = req.body;
+  console.log("new note: ", newNote);
+  return noteData
+    .update(index, newNote)
+    .then(() => noteData.list())
+    .then((notes) => {
+      res.json("updated");
+      res.status(200).json(notes);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/note/:index", (req, res) => {
+  console.log("deleting json file");
+  let index = req.params.index;
+  return noteData
+    .remove(index)
+    .then(() => noteData.list())
+    .then((notes) => {
+      res.json("Deleted");
+      res.status(200).json(notes);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log("Running on port ", port);
+});
---
+++ note.js
@@ -0,0 +1,83 @@
+var fs = require("fs");
+
+module.exports = class Note {
+  constructor(file) {
+    this.file = file;
+    this.initPromise = null;
+    this.init();
+  }
+  init() {
+    if (this.initPromise === null) {
+      this.initPromise = new Promise((res, rej) => {
+        this.read()
+          .then(() => {
+            res();
+          })
+          .catch(() => {
+            this.note = [];
+            this.write().then(res).catch(rej);
+          });
+      });
+    }
+    return this.initPromise;
+  }
+  read() {
+    return new Promise((res, rej) => {
+      fs.readFile(this.file, "utf-8", (err, data) => {
+        if (err) {
+          rej(err);
+        }
+        try {
+          this.note = JSON.parse(data);
+        } catch (err) {
+          rej(err);
+        }
+        res(this.note);
+      });
+    });
+  }
+  write() {
+    return new Promise((res, rej) => {
+      fs.writeFile(this.file, JSON.stringify(this.note), (err) => {
+        if (err) {
+          rej(err);
+        }
+        res(this.note);
+      });
+    });
+  }
+
+  add(note) {
+    return this.init().then(() => {
+      return this.read().then(() => {
+        console.log("Adding note: ", note);
+        this.note.push(note);
+        return this.write();
+      });
+    });
+  }
+
+  update(index, newNote) {
+    return this.init().then(() => {
+      return this.read().then(() => {
+        console.log(`updating note at index ${index} with ${newNote}`);
+        this.note[index] = newNote;
+        return this.write();
+      });
+    });
+  }
+  remove(index) {
+    return this.init().then(() => {
+      return this.read().then(() => {
+        console.log("Note to delete: ", this.note[index]);
+        this.note.splice(index, 1);
+        return this.write();
+      });
+    });
+  }
+  list() {
+    return this.init().then(() => {
+      return this.read();
+    });
+  }
+};
