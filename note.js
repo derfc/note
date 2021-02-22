069291fccd4b09fd5ee9a5d6ae1fbddd4f47ae59
derfc <fredcheung@live.hk>
2021-02-22 19:34:36 +0800

    backend done

--- note.js
+++ note.js
@@ -1,93 +1,99 @@
 var fs = require("fs");
 
 module.exports = class Note {
-  constructor(file) {
-    this.file = file;
-    this.initPromise = null;
-    this.init();
-  }
-  init() {
-    if (this.initPromise === null) {
-      this.initPromise = new Promise((res, rej) => {
-        this.read()
-          .then(() => {
-            res();
-          })
-          .catch(() => {
-            this.note = [];
-            this.write().then(res).catch(rej);
-          });
-      });
-    }
-    return this.initPromise;
-  }
-  read() {
-    return new Promise((res, rej) => {
-      fs.readFile(this.file, "utf-8", (err, data) => {
-        if (err) {
-          rej(err);
-        }
-        try {
-          this.note = JSON.parse(data);
-        } catch (err) {
-          rej(err);
-        }
-        res(this.note);
-      });
-    });
-  }
-  write() {
-    return new Promise((res, rej) => {
-      fs.writeFile(this.file, JSON.stringify(this.note), (err) => {
-        if (err) {
-          rej(err);
-        }
-        res(this.note);
-      });
-    });
-  }
+	constructor(file) {
+		this.file = file;
+		this.initPromise = null;
+		this.init();
+	}
+	init() {
+		if (this.initPromise === null) {
+			this.initPromise = new Promise((res, rej) => {
+				this.read()
+					.then(() => {
+						res();
+					})
+					.catch(() => {
+						this.note = [];
+						this.write().then(res).catch(rej);
+					});
+			});
+		}
+		return this.initPromise;
+	}
+	read() {
+		return new Promise((res, rej) => {
+			fs.readFile(this.file, "utf-8", (err, data) => {
+				if (err) {
+					rej(err);
+				}
+				try {
+					this.note = JSON.parse(data);
+				} catch (err) {
+					rej(err);
+				}
+				res(this.note);
+			});
+		});
+	}
+	write() {
+		return new Promise((res, rej) => {
+			fs.writeFile(this.file, JSON.stringify(this.note), (err) => {
+				if (err) {
+					rej(err);
+				}
+				res(this.note);
+			});
+		});
+	}
 
-  add(note) {
-    return this.init().then(() => {
-      return this.read().then(() => {
-        console.log("Adding note: ", note);
-        this.note.push(note);
-        return this.write();
-      });
-    });
-  }
+	add(note, userName) {
+		return this.init().then(() => {
+			if (typeof this.note[userName] == "undefined") {
+				this.note[userName] = [];
+			}
+			this.note[userName].push(note);
+			return this.write();
+		});
+	}
 
-  update(index, newNote) {
-    return this.init().then(() => {
-      return this.read().then(() => {
-        console.log(`updating note at index ${index} with ${newNote}`);
-        this.note[index] = newNote;
-        return this.write();
-      });
-    });
-  }
-  remove(userName, index) {
-    return this.init().then(() => {
-      return this.read().then(() => {
-        // console.log("Note to delete: ", this.note[index]);
+	addUser(user) {
+		return this.init().then(() => {
+			return this.read().then(() => {
+				console.log("Adding user: ", user);
+				this.note.push(user);
+				return this.write();
+			});
+		});
+	}
 
-        let notes = [];
-        for (let i = 0; i < this.note.length; i++) {
-          if (Object.keys(this.note[i]).includes(userName)) {
-            notes.push(this.note[i].userName);
-          }
-        }
-        console.log(notes);
-        let targetIndex = this.note.indexOf(notes[index]);
-
-        this.note.splice(targetIndex, 1);
-        return this.write();
-      });
-    });
-  }
-  list() {
-    return this.init().then(() => {
-      return this.read();
-    });
-  }
+	update(userName, index, newNote) {
+		return this.init().then(() => {
+			return this.read().then(() => {
+				console.log(this.note);
+				console.log("wtf", userName);
+				console.log(index);
+				console.log(newNote);
+				this.note[userName][index] = newNote;
+				return this.write();
+			});
+		});
+	}
+	remove(userName, index) {
+		return this.init().then(() => {
+			return this.read().then(() => {
+				// console.log("Note to delete: ", this.note[index]);
+				console.log(this.note);
+				console.log(userName);
+				console.log(index);
+				this.note[userName].splice(index, 1);
+				return this.write();
+			});
+		});
+	}
+	list() {
+		return this.init().then(() => {
+			return this.read();
+		});
+	}
 };
--- working.js
+++ working.js
@@ -8,9 +8,9 @@ var hb = require("express-handlebars");
 var bcrypt = require("bcrypt");
 var app = express();
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 app.use(bodyParser.json());
 app.use(cors());
@@ -25,85 +25,112 @@ let noteData = new Note(jsonFile);
 var port = 8080;
 
 app.get("/", (req, res) => {
-  console.log("diu");
+	console.log("diu");
 
-  res.render("index");
-  console.log(req);
+	res.render("index");
+	console.log(req);
 });
 app.get("/users", (req, res) => {
-  // res.json(users);
-  console.log("getting users");
-  return usersData
-    .list()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	// res.json(users);
+	console.log("getting users");
+	return usersData
+		.list()
+		.then((users) => {
+			res.json(users);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 app.post("/users/login", async (req, res) => {
-  console.log(req.body);
-  let userName = req.body.username;
-  let existingUser = fs.readFileSync(
-    "./users.json",
-    "utf-8",
-    async (err, data) => {
-      if (err) {
-        throw err;
-      }
-      return await data;
-    }
-  );
-  let parsed = JSON.parse(existingUser);
-  let user = parsed.find((user) => user.name === userName);
-  if (user == null) {
-    try {
-      let salt = await bcrypt.genSalt();
-      let hashedPassword = await bcrypt.hash(req.body.password, salt);
-      console.log(salt);
-      console.log(hashedPassword);
-      let user = { name: userName, password: hashedPassword };
-      console.log(user);
-      return usersData
-        .add(user)
-        .then(() => {
-          usersData.list();
-        })
-        .then((users) => {
-          res.redirect(`http://localhost:${port}/users/${userName}`);
-          // res.render("usersHome", {
-          //   title: userName,
-          //   userName: userName,
-          // });
-          // res.status(200).json(users);
-        });
-    } catch {
-      res.status(500).send();
-    }
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      // res.send("success");
-      res.redirect(`http://localhost:${port}/users/${userName}`);
-      // .render("usersHome", {
-      //   title: userName,
-      //   userName: userName,
-      // });
-    } else res.send("wrong password");
-  } catch {
-    res.status(500).send();
-  }
+	console.log(req.body);
+	let userName = req.body.username;
+	let existingUser = fs.readFileSync(
+		"./users.json",
+		"utf-8",
+		async (err, data) => {
+			if (err) {
+				throw err;
+			}
+			return await data;
+		}
+	);
+	let parsed = JSON.parse(existingUser);
+	let user = parsed.find((user) => user.name === userName);
+	if (user == null) {
+		try {
+			let salt = await bcrypt.genSalt();
+			let hashedPassword = await bcrypt.hash(req.body.password, salt);
+			console.log(salt);
+			console.log(hashedPassword);
+			let user = { name: userName, password: hashedPassword };
+			console.log(user);
+			return usersData
+				.addUser(user)
+				.then(() => {
+					usersData.list();
+				})
+				.then((users) => {
+					res.redirect(`/users/${userName}`);
+					// res.render("usersHome", {
+					//   title: userName,
+					//   userName: userName,
+					// });
+					// res.status(200).json(users);
+				});
+		} catch {
+			res.status(500).send();
+		}
+	}
+	try {
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			// res.send("success");
+			res.redirect(`/users/${userName}`);
+			// .render("usersHome", {
+			//   title: userName,
+			//   userName: userName,
+			// });
+		} else res.send("wrong password");
+	} catch {
+		res.status(500).send();
+	}
 });
 
 app.get("/users/:username", (req, res) => {
-  console.log("still here");
-  console.log(req.params.username);
-  res.render("usersHome", {
-    title: req.params.username,
-    userName: req.params.username,
-  });
+	// console.log("still here");
+	// console.log(req.params.username);
+	// res.render("usersHome", {
+	// 	title: req.params.username,
+	// 	userName: req.params.username,
+	// 	port: port,
+	// });
+
+	console.log("getting json file");
+	console.log(req.params.username);
+	return noteData
+		.list()
+		.then((notes) => {
+			console.log("hi", notes[req.params.username]);
+			const allNotes = {
+				note: notes[req.params.username],
+				title: req.params.username,
+				userName: req.params.username,
+				port: port,
+			};
+			console.log("export", allNotes);
+			// res.render("usersHome", {
+			// 	title: req.params.username,
+			// 	userName: req.params.username,
+			// 	port: port,
+			// 	file: notes,
+			// });
+
+			res.render("usersHome", allNotes);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 // app.post("/users/login", async (req, res) => {
@@ -132,71 +159,97 @@ app.get("/users/:username", (req, res) => {
 // });
 
 app.get("/note", (req, res) => {
-  console.log("getting json file");
-  return noteData
-    .list()
-    .then((notes) => {
-      res.json(notes);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	console.log("getting json file");
+	return noteData
+		.list()
+		.then((notes) => {
+			res.json(notes);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-app.get("/note/:id", (req, res) => {
-  // res.json(noteStore[req.params.id]);
-});
+// app.get("/user/:username", (req, res) => {
+// 	// res.json(noteStore[req.params.id]);
+// 	console.log("getting json file");
+// 	console.log(req.params.username);
+// 	return noteData
+// 		.list()
+// 		.then((notes) => {
+// 			console.log(notes);
+// 			res.render("usersHome", {
+// 				title: req.params.username,
+// 				userName: req.params.username,
+// 				port: port,
+// 				note: { note },
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
 
 app.post("/note", (req, res) => {
-  console.log("posting json file");
-  console.log(req.body);
-  let userName = Object.keys(req.body)[0];
-  let note = req.body;
-  console.log("posting ", note);
-  return noteData
-    .add(note)
-    .then(() => noteData.list())
-    .then((notes) => {
-      res.redirect(`http://localhost:${port}/users/${userName}`);
-      // res.json("posted");
-      // res.status(200).json(notes);
-    });
+	console.log("posting json file");
+	console.log(req.body);
+	let note = req.body;
+	let userName = Object.keys(req.body)[0];
+	console.log("username", userName);
+	console.log("reqbody", note);
+	console.log("posting ", note[Object.keys(req.body)[0]]);
+	let adding = note[Object.keys(req.body)[0]];
+	return noteData
+		.add(adding, userName)
+		.then(() => noteData.list())
+		.then((notes) => {
+			console.log("here");
+			res.redirect(`/users/${userName}`);
+			// res.json("posted");
+			// res.status(200).json(notes);
+		});
 });
 
-app.put("/note/:index", (req, res) => {
-  console.log("updating json file");
-  let index = req.params.index;
-  console.log("index: ", index);
-  let newNote = req.body;
-  console.log("new note: ", newNote);
-  return noteData
-    .update(index, newNote)
-    .then(() => noteData.list())
-    .then((notes) => {
-      res.json("updated");
-      // res.status(200).json(notes);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.put("/note/:username/:value/:index", (req, res) => {
+	console.log("updating json file");
+	let index = req.params.index;
+	console.log("index: ", index);
+	let userName = req.params.username;
+	console.log("user ", userName);
+	let newNote = req.params.value;
+	console.log(newNote);
+	return noteData
+		.update(userName, index, newNote)
+		.then(() => noteData.list())
+		.then((notes) => {
+			res.json("updated");
+			// res.status(200).json(notes);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 app.delete("/note/:username/:index", (req, res) => {
-  console.log("deleting json file");
-  let userName = req.params.username;
-  let index = req.params.index;
-  return noteData
-    .remove(userName, index)
-    .then(() => noteData.list())
-    .then((notes) => {
-      res.redirect(`http://localhost:${port}/users/${userName}`);
-      // res.status(200).json(notes);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	console.log("deleting json file");
+	let userName = req.params.username;
+	let index = req.params.index;
+	return noteData
+		.remove(userName, index)
+		.then(() => noteData.list())
+		.then((notes) => {
+			console.log("trying redirect");
+			console.log(notes);
+			// res.redirect(`/users/${userName}`);
+			res.json(notes);
+			// res.render('index', {notes})
+			// res.status(200).json(notes);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 app.listen(port, () => {
-  console.log("Running on port ", port);
+	console.log("Running on port ", port);
 });
